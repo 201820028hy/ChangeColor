7fe69d4b815def5e55e2793aaf108becd037c48a
dit03 <dit@192.168.nate.com>
2019-03-12 15:42:35 +0900

    Change Background 과제

--- ChangeColor/ViewController.swift
+++ ChangeColor/ViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var btnRed: UIButton!
+    @IBOutlet weak var btnYellow: UIButton!
+    @IBOutlet weak var btnGreen: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    @IBAction func changeRed(_ sender: Any) {
+        self.view.backgroundColor = UIColor.red
+    }
+    
+    @IBAction func changeYellow(_ sender: Any) {
+        self.view.backgroundColor = UIColor.yellow
+    }
+    
+    @IBAction func changeGreen(_ sender: Any) {
+        self.view.backgroundColor = UIColor.green
+    }
+    
 }
 
